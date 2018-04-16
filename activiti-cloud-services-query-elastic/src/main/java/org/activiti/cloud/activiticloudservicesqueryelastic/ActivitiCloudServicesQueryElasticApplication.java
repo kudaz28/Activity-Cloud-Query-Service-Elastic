0c097a7b9a67ef9e038cb31c8c5db42c97c11936
William Zhou <williamkzhou@hotmail.com>
2018-04-16 16:27:11 +0100

    Application files

---
+++ activiti-cloud-services-query-elastic/src/main/java/org/activiti/cloud/activiticloudservicesqueryelastic/ActivitiCloudServicesQueryElasticApplication.java
@@ -0,0 +1,12 @@
+package org.activiti.cloud.activiticloudservicesqueryelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiCloudServicesQueryElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiCloudServicesQueryElasticApplication.class, args);
+	}
+}
---
+++ activiti-cloud-services-query-elastic/src/test/java/org/activiti/cloud/activiticloudservicesqueryelastic/ActivitiCloudServicesQueryElasticApplicationTests.java
@@ -0,0 +1,16 @@
+package org.activiti.cloud.activiticloudservicesqueryelastic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivitiCloudServicesQueryElasticApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
